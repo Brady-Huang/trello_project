01043101637adef8c83345e6f926806b8ede99c4
Brady <brady120915@gmail.com>
2022-11-30 14:23:38 +0800

    get list data to frontend via vue2.js

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -4,6 +4,20 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
 import "../styles/"
+
+//vue.js
+import Vue from 'vue/dist/vue.esm';
+document.addEventListener("turbolinks:load", function(event) {
+  let el = document.querySelector('#board');
+  if (el) {
+    new Vue({
+      el,
+      data: {
+        lists: JSON.parse(el.dataset.lists)
+      }
+    });
+  }
+})
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
